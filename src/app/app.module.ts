8d4fc622175e4011d647148fb74d05854bb8b066
Renaldy002 <oshaleon581@Gmail.com>
2023-10-27 21:28:14 +0700

    update app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,21 @@ import { NgClass } from '@angular/common';
 import { OurExperiencesComponent } from './our-experiences/our-experiences.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeBackgroundComponent } from './home-background/home-background.component';
+import { OurCompanyComponent } from './our-company/our-company.component';
+import { OurManagementComponent } from './our-management/our-management.component';
+import { OurClientsComponent } from './our-clients/our-clients.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OurExperiencesComponent,
     ContactComponent,
-    FooterComponent
+    FooterComponent,
+    HomeBackgroundComponent,
+    OurCompanyComponent,
+    OurManagementComponent,
+    OurClientsComponent
   ],
   imports: [
     BrowserModule,
